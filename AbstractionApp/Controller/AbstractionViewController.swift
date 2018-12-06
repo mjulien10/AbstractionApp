d8d1587cc14cd0d60c4286e6068cc7470a0bf0e8
Michael <mjmjulien10@gmail.com>
2018-12-06 08:09:03 -0700

    put images into assests.xcassets

---
+++ AbstractionApp/Controller/AbstractionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AbstractionViewController.swift
+//  AbstractionApp
+//
+//  Created by Julien, Michael on 12/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class AbstractionViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
