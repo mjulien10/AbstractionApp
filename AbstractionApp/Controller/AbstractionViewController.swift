1012b56613c8f3282b123eb732bc30ccd9f6a089
Michael <mjmjulien10@gmail.com>
2018-12-07 07:38:46 -0700

    epic commit

--- AbstractionApp/Controller/AbstractionViewController.swift
+++ AbstractionApp/Controller/AbstractionViewController.swift
@@ -8,23 +8,92 @@
 
 import UIKit
 
-class AbstractionViewController: UIPageViewController {
+public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
+{
 
-    override func viewDidLoad() {
+    public override func viewDidLoad()
+    {
         super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
 
-        // Do any additional setup after loading the view.
+    //MARK:- Data members
+    private lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "Swift"),
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "LogicalGate")
+        ]
+    }()
+    
+    //MARK:- Helper method to retrieve the correct ViewController based on the data member
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    //SWIPE RIGHT METHOD
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController:
+        UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        
+            else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+            else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[previousIndex]
     }
-    */
+    
+    //SWIPE LEFT METHOD
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController:
+        UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            
+            else
+        {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex >= 0
+            else
+        {
+            return nil
+        }
+        
+        guard nextIndex < orderedAbstractionViews.count
+            else
+        {
+            return orderedAbstractionViews.first
+        }
+        
+        return orderedAbstractionViews[nextIndex]
+    }
 
 }
